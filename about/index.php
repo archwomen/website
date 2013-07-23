fdd177e5e9ec3e0f39f96622269028ae9117220c
Dolores Portalatin <admin@doloresportalatin.info>
2013-07-23 16:35:41 -0400

    Added information to the about page

--- about/index.php
+++ about/index.php
@@ -18,7 +18,37 @@
 	        <div class="col-1-1">
 		        <div class="content">
 					<h2>About Arch Women</h2>
-                    <p>text here</p>
+                    <p>The goal of Arch Women is to foster female involvement in 
+                    Arch Linux and Free Open Source Software. The community is open 
+                    to female Linux enthusiasts and users, and to the men who support them.
+                    If you would like to join us, pleaes have a look at our community
+                    standards to get a feel for how our community is run.</p>
+                    <p>Arch Women has a monthly IRC meeting on the first sunday of
+                    every month. They genreally run for about 2 hours and start at 16:00 UTC.</p>
+                    <p>The Arch Women <a href="/blog">Blog</a> is a place for discussion and the sharing of 
+                    information in a contained and moderated way. <a href="/blog/submit">Submissions</a> 
+                    are welcome from anyone who like to share something.</p>
+                    <p>The #archlinux-women channel was created to provide a 
+                    "safe space" for people to chat without the need to hide their 
+                    gender or sexual identity. We also have other ways you can 
+                    <a href="/contact">contact us</a>.</p>
+                    <h2>History</h2>
+                    <p>Arch Women was founded in February 2012 by Dolores Portalatin 
+                    &emdash; "meskarune". It sprang from an IRC 
+                    conversation in #archlinux-offtopic about how few women 
+                    openly contribute to the Arch Linux community. From there an 
+                    IRC channel (#archlinux-women) was created for planning, a few 
+                    long time Arch Linux users joined in, and two days later the 
+                    main website was up and running. Since that time, Arch Women 
+                    has changed servers twice and tripled in membership.</p>
+                    <h2>Leaders and Moderators</h2>
+                    <h3>IRC Channel Moderators</h3>
+                    <h3>Website Administrators</h3>
+                    <h2>Project Afiliations</h2>
+                    <ul>
+                        <li></li>
+                        <li></li>
+                    </ul>
 	            </div>
 	        </div>
         </div>	    
