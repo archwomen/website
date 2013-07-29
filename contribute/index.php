c525f50285ee788401bf910b0739aa817417a8de
Dolores Portalatin <meskarune@archwomen.org>
2013-07-29 10:02:09 -0400

    Added some information to the "contribute" page

--- contribute/index.php
+++ contribute/index.php
@@ -5,17 +5,43 @@
         <meta name="description" content="Organizational website for Arch Women">
         <meta name="author" content="Arch Women">
         <meta name="keywords" lang="en-us" content="linux, arch linux, women, arch women, technology, programming, feminism">
-	    <meta charset="utf-8">
-	    <meta name="viewport" content="width=device-width,initial-scale=1">
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width,initial-scale=1">
         
-		<link rel="stylesheet" type="text/css" media"all" href="/assets/normalize.css">
-		<link rel="stylesheet" type="text/css" media="screen" href="/assets/screen.css">
+	<link rel="stylesheet" type="text/css" media"all" href="/assets/normalize.css">
+	<link rel="stylesheet" type="text/css" media="screen" href="/assets/screen.css">
         <link rel="stylesheet" type="text/css" media="print" href=/assets/print.css">
     </head>
     <body>
         <?php include '../assets/header.html'; ?>
         <article>
-		    <p>test</p>
+            <h2>Contribute to Arch Linux Women</h2>
+	        <p>Arch Linux Women is community driven, and without contributions, the project will die. Even small 
+                contributions add up to something great, so please get involved where you can. There are a ton of ways 
+                that both new and old members can contribute, and you don't have to be a programmer or have large amounts
+                of free time to help.</p>
+                <p>A few of the ways you can contribute include:</p>
+            <h3>Tell others about the project</h3>
+                <p>If you know any women who use Linux, or even just might be interested in learning, tell them about our
+                project. Mention us in one of your social media updates or make a blog post about our organization. Print out 
+                some of our flyers and post them up around your College CS lab. The first step to getting more people involved 
+                with Arch Linux Women, is telling them about Arch Linux Women.</p>
+            <h3>Chat in our IRC channels</h3>                
+                <p>Arch Women has two IRC channels on irc.freenode.net - #archlinux-women and #archlinux-women-dev. 
+                Before joining in please, please read the channel rules and community standards. Our main chat is used 
+                for networking, learning and discussion. Our developement chat is used for Arch Women project developement,
+                such as the IRC bot, the website, and server tools. You can jump in there to see our git commits and learn
+                how things are done "behind the scenes".</p>
+            <h3>Take part in our monthy meeting</h3>
+            <h3>Join our mailinglist</h3>
+            <h3>Contribute to the Wiki</h3>
+            <h3>Submit a blog post</h3>
+            <h3>Create Artwork or Flyers</h3>
+            <h3>Mentor other members</h3>
+            <h3>Teach a class</h3>
+            <h3>Help with Software Developement</h3>
+            <h3>Donate</h3>
+            <h3>Sponsor us</h3>
         </article>	    
         <?php include '../assets/footer.html'; ?>
     </body>
