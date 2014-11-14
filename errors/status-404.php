bfb551e772361b2b887dc52f12a12ffe65cc03aa
Dolores Portalatin <meskarune@gmail.com>
2014-11-13 22:02:00 -0500

    Added directory listing css theme and 404 error template

---
+++ errors/status-404.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Arch Linux Women | 404</title>
+        <meta name="description" content="Organizational website for Arch Women">
+        <meta name="author" content="Arch Women">
+        <meta name="keywords" lang="en-us" content="linux, arch linux, women, arch women, technology, programming, feminism">
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width,initial-scale=1">
+
+        <link rel="stylesheet" type="text/css" media"all" href="../assets/normalize.css">
+        <link rel="stylesheet" type="text/css" media="screen" href="../assets/screen.css">
+    </head>
+    <body>
+        <?php include '../assets/header.html'; ?>
+        <article>
+            <h1>404</h1>
+            <h4>I'm sorry, the page you are looking for does not exist.</h4>
+        </article>
+        <?php include '../assets/footer.html'; ?>
+    </body>
+</html>
