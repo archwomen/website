4f6b0e77d3410b4692394b98aad7aa9aa293e57b
Dolores Portalatin <meskarune@gmail.com>
2014-11-15 00:01:36 -0500

    added some text to community standards page

--- community-standards/index.php
+++ community-standards/index.php
@@ -3,6 +3,19 @@
   include $_SERVER['DOCUMENT_ROOT']."/inc/header.php";
 ?>
       <article>
-        <p>Under Construction/Review</p>
+        <h2>Community Standards</h2>
+        <p>Arch Women contains a diverse group of people who have come together to 
+        lower the barriers for women to contribute to Arch Linux and FOSS as well as
+        make friends and have fun. Our community standards are a set of guidelines for 
+        members to follow when they interact with each other.</p>
+        <p>Our online community includes the main archwomen.org website, social media groups, 
+        and our IRC channels. We expect community members to be helpful and
+        respectful. Treat others how you would like to be treated and take responsibility
+        for your words and actions. Spamming, trolling, harassment, bullying, 
+        discrimination and threatening behavior will not be tolerated.</p>
+        <h2>Additional rules and guidelines</h2>
+        <ul>
+          <li><a href="/wiki/aw-org:irc">IRC channel rules</a></li>
+        </ul>
       </article>
 <?php include $_SERVER['DOCUMENT_ROOT']."/inc/footer.php"; ?>
