ad7ef547972fa18d60731d6393bfdadf72eb9241
Jenny Wong <jy2wong@csclub.uwaterloo.ca>
2015-01-04 18:11:34 -0500

    border="0" -> border: 0; css on "contribute" page

--- contribute/index.php
+++ contribute/index.php
@@ -26,8 +26,8 @@
         <form style="display:inline;" action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
           <input type="hidden" name="cmd" value="_s-xclick">
           <input type="hidden" name="hosted_button_id" value="ADCGV6K9VK2MJ">
-          <input type="image" src="https://archwomen.org/assets/images/icons/coffee.png" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
-          <img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
+          <input type="image" src="https://archwomen.org/assets/images/icons/coffee.png" name="submit" alt="PayPal - The safer, easier way to pay online!">
+          <img alt="" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
         </form>
         <p>Current Costs:
           <ul>
