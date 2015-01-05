a80373ca6d361e1dd0a9eea6b9e87badf98881c2
CTK <calimeroteknik@free.fr>
2015-01-05 22:17:51 +0100

    index.php:  adjusted the presentation text  changed 'Get involved' to a small text

--- index.php
+++ index.php
@@ -5,39 +5,30 @@
       <section class="col-1-2">
         <h2 role="heading">Welcome!</h2>
         <img alt="W icon" class="left" src="assets/images/ArchW.png">
-        <p class="justify">You've reached the site for the Arch Linux Women project. 
-        Arch Linux Women (Arch Women or A-W for short) is an all inclusive 
-        organization of Arch Linux enthusiasts with a focus on helping more
-        women become involved in the Arch Linux community and 
-        <abbr title="free open source software"> FOSS</abbr>. To do 
-        this, we provide a safe space for people of all genders and 
-        backgrounds to share skills, learn, network and socialize. 
-        Anyone with an interest in our mission is welcome to participate.</p>
+        <p>
+          Arch Linux Women (Arch Women or A-W for short) is an all inclusive 
+          organization of Arch Linux enthusiasts with a focus on helping more 
+          women become involved in the Arch Linux community and 
+          <abbr title="free open source software"> FOSS</abbr>.
+        </p>
+        <p>
+          To this end, we provide a safe space for people of all genders and 
+          backgrounds to share skills, learn, network and socialize. 
+          Anyone with an interest in the project is welcome to participate.
+        </p>
       </section>
       <section class="col-2-2">
         <h2 role="heading">Get Involved</h2>
-        <ul class="left">
-          <li>
-            <a href="/join">Join</a>
-            <ul>
-              <li><a href="/join/#women">Women</a></li>
-              <li><a href="/join/#ally">Allies</a></li>
-            </ul>
-          </li>
-          <li>Spread the word</li>
-          <li>Sponsor us</li>
-        </ul>
-        <ul class="left">
-          <li>
-            <a href="/contribute">Contribute to the project</a>
-            <ul>
-              <li><a href="/blog">Blog</a></li>
-              <li><a href="/wiki">Wiki</a></li>
-              <li>Teach</li>
-              <li><a href="https://github.com/archwomen">Github</a></li>
-            </ul>
-          </li>
-        </ul>
+        <p>
+          Do you want to
+          <a href="https://wiki.archlinux.org/index.php/Classroom">teach a class</a>, organize an event, 
+          <a href="https://github.com/archwomen">write code</a>,
+          or simply join the conversation? 
+          See how you can <a href="/contribute">contribute</a> to the project!
+        </p>
+        <p>
+          We are also looking for sponsors and donations.
+        </p>
       </section>
     </article>
     <article class="clearfix">
@@ -45,11 +36,11 @@
         <h2 role="heading">Projects</h2> 
         <h3><a href="https://wiki.archlinux.org/index.php/Classroom">Classroom</a></h3>
         <p>Arch Linux Classroom is a project to develop and host classes and
-        teaching materials that can be used by the Arch Linux community.<br />
-        <a href="https://wiki.archlinux.org/index.php/Classroom">Read more!</a></p>
+        teaching materials that can be used by the Arch Linux community.
+        <a href="https://wiki.archlinux.org/index.php/Classroom">Read&nbsp;more!</a></p>
         <h3><a href="https://archwomen.org/wiki/projects:internships:start">Internship</a></h3>
-        <p>Internships to work on Arch Linux development: GSoC, OPW.<br />
-        <a href="https://archwomen.org/wiki/projects:internships:start">Read more!</a></p>
+        <p>Internships to work on Arch Linux development: GSoC, OPW.
+        <a href="https://archwomen.org/wiki/projects:internships:start">Read&nbsp;more!</a></p>
         <h3>Mentor Connection</h3>
       </section>
       <?php
