8fddb8b0acf884c8a6e274671a7f4557d8a3fcb0
Dolores Portalatin <meskarune@gmail.com>
2015-02-08 20:23:32 -0500

    Added Donation page and thanks page

---
+++ donate/index.php
@@ -0,0 +1,10 @@
+<?php
+  $title = "Community Standards";
+  include $_SERVER['DOCUMENT_ROOT']."/inc/header.php";
+?>
+      <article role="main">
+        <h2 role="heading">Donate to Arch Women</h2>
+        <p>It takes people like you to keep our organization going. Your donation will 
+        help pay for our hosting costs and the services we provide for Arch Linux users.</p>
+      </article>
+<?php include $_SERVER['DOCUMENT_ROOT']."/inc/footer.php"; ?>
---
+++ donate/thanks.php
@@ -0,0 +1,8 @@
+<?php
+  $title = "Community Standards";
+  include $_SERVER['DOCUMENT_ROOT']."/inc/header.php";
+?>
+      <article role="main">
+        <h2 role="heading">Thank you for your contribution</h2>
+      </article>
+<?php include $_SERVER['DOCUMENT_ROOT']."/inc/footer.php"; ?>
