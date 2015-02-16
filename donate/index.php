59a089b4cde770243d47b314a57d4de9f5d2eef5
Dolores Portalatin <meskarune@gmail.com>
2015-02-15 19:10:31 -0500

    added css to style form

--- donate/index.php
+++ donate/index.php
@@ -1,6 +1,6 @@
 <?php
   $title = "Community Standards";
-  $custom = "<link rel='stylesheet' href='odometer-theme-default.css'>";
+  $custom = "<style>.form-inline .form-group {display: inline-block;margin-bottom: 0;vertical-align: middle;}</style><link rel='stylesheet' href='odometer-theme-default.css'>";
   include $_SERVER['DOCUMENT_ROOT']."/inc/header.php";
 ?>
       <article role="main">
