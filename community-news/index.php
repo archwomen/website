d7c6169ad9117a24d696ac7c676a23ea47fabe6a
Dolores Portalatin <meskarune@gmail.com>
2015-03-22 23:20:05 -0400

    added pagination code

--- community-news/index.php
+++ community-news/index.php
@@ -9,10 +9,13 @@
   $feed->set_cache_location($_SERVER['DOCUMENT_ROOT'] . '/community-news/cache');
   $feed->enable_cache();
   $feed->init();
+  $max = $feed->get_item_quantity();
+  $start = (isset($_GET['page']) && !empty($_GET['page'])) ? $_GET['page'] : 0;
+  $entries = 6; //entries per page
 ?>
     <div id="content">
       <h2 role="heading">Community News</h2>
-      <?php foreach ($feed->get_items(0, 20) as $item): ?>
+      <?php foreach ($feed->get_items($start, $entries) as $item): ?>
         <article class="feed">
         <?php
           if ($item->get_permalink()) echo '<a href="' . $item->get_permalink() . '">';
@@ -28,5 +31,23 @@
           <p><?php print $item->get_content(false); ?></p>
         </article>
       <?php endforeach; ?>
+      <?php
+        $next = (int) $start + $entries;
+        $prev = (int) $start - $entries;
+        $nextlink = '<a href="?page=' . $next . '"> Next &raquo; </a>';
+        if ($next > $max) {
+          $nextlink = ' Next &raquo; ';
+        }
+        $prevlink = '<a href="?start=' . $prev . '"> &laquo; Previous </a>';
+        if ($prev < 0 && (int) $start > 0) {
+          $prevlink = '<a href="?start=0">&laquo; Previous </a>';
+        }
+        else if ($prev < 0) {
+          $prevlink = '&laquo; Previous ';
+        }
+        $begin = (int) $start + 1;
+        $end = ($next > $max) ? $max : $next;
+        echo '<p>' . $prevlink . $nextlink . '</p>';
+      ?>
     </div>
 <?php include $_SERVER['DOCUMENT_ROOT']."/inc/footer.php"; ?>
