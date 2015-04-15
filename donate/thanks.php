d3d8e987d88c779d9f5165d516cb480c3ffe9fa1
Dolores Portalatin <meskarune@gmail.com>
2015-04-15 11:26:35 -0400

    updated thank you page

--- donate/thanks.php
+++ donate/thanks.php
@@ -3,6 +3,10 @@
   include $_SERVER['DOCUMENT_ROOT']."/inc/header.php";
 ?>
       <article role="main">
-        <h2 role="heading">Thank you for your contribution</h2>
+        <h2 role="heading">Thank you for your contribution!</h2>
+        <p>It takes a community coming together to make change for the better.
+        Thank you for supporting us, we really appreciate it.</p>
+        <p>If you would like to contact us with any concerns or feedback, please 
+        email admin @ archwomen.org</p>
       </article>
 <?php include $_SERVER['DOCUMENT_ROOT']."/inc/footer.php"; ?>
